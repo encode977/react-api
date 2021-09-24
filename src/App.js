b4a839519518b828b3fccdf0b35afebc15c2180a
Lokindra Dangi <pachar.com@gmail.com>
2021-09-24 22:41:50 +0545

    POST request added

--- src/App.js
+++ src/App.js
@@ -6,40 +6,67 @@ function App() {
   const [data, setData] = useState();
 
   useEffect(() => {
-    const fetchData = async () => {
-      //=========== GET with Async await=========
-      // const result = await axios.get(
-      //   "https://jsonplaceholder.typicode.com/todos"
-      // );
-      // const { data } = result;
-      // console.log(data);
-      // setData(data);
+    //const fetchData = async () => {
+    //=========== GET with Async await=========
+    // const result = await axios.get(
+    //   "https://jsonplaceholder.typicode.com/todos"
+    // );
+    // const { data } = result;
+    // console.log(data);
+    // setData(data);
 
-      //==========Async awit error handling===========
+    //==========Async awit error handling===========
+    // try {
+    //   const result = await axios.get(
+    //     "https://jsonplaceholder.typicode.com/todos"
+    //   );
+    //   const { data } = result;
+    //   const value = data.data;
+    //   setData(value);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    // =================GET with Promises(.then)==========
+    //   axios
+    //     .get("https://jsonplaceholder.typicode.com/todos")
+    //     .then((response) => {
+    //       const { data } = response;
+    //       console.log(data);
+    //       setData(data);
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     });
+    // };
+
+    const postData = async () => {
+      //========== POST Async awit error handling===========
       // try {
-      //   const result = await axios.get(
-      //     "https://jsonplaceholder.typicode.com/todos"
+      //   const data = {
+      //     title: "NJ is the Best Striking in PSG",
+      //     completed: true,
+      //   };
+      //   const response = await axios.post(
+      //     "https://jsonplaceholder.typicode.com/todos",
+      //     data
       //   );
-      //   const { data } = result;
-      //   const value = data.data;
-      //   setData(value);
+      //   console.log(response.data);
       // } catch (error) {
       //   console.log(error);
       // }
-      // =================GET with Promises(.then)==========
-      axios
-        .get("https://jsonplaceholder.typicode.com/todos")
-        .then((response) => {
-          const { data } = response;
-          console.log(data);
-          setData(data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      // =================POST with Promises(.then)==========
+      // axios({
+      //   method: "POST",
+      //   url: "https://jsonplaceholder.typicode.com/todos",
+      //   data: { title: "PSG is the best team", completed: true },
+      // }).then((response) => {
+      //   const { data } = response;
+      //   console.log(data);
+      // });
     };
 
-    fetchData();
+    // fetchData();
+    postData();
   }, []);
 
   return (
